62520e758d6d375680de78e19cd7ba9824d8c8b0
SahanRathnaweera <sahantharuka0909@gmail.com>
2026-09-17 21:31:13 +0530

    Update : update UI colour change

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Colors.indigo;
+  static const Color primary = Color.fromARGB(255, 41, 53, 124);
   static Color background = Colors.indigo.shade400;
   static Color buttonGrey = Colors.grey.shade700;
   static const Color buttonRed = Colors.red;
